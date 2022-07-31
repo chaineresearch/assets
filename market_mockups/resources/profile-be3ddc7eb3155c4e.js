024d134b3c9dc9e334e41697c5f2e71baaea4a91
kitblake <kitblake@infrae.com>
2022-08-01 01:55:41 +0900

    Rename resources dir

---
+++ market_mockups/resources/profile-be3ddc7eb3155c4e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[277],{99813:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/profile",function(){return u(26480)}])}},function(n){n.O(0,[966,951,44,766,3,674,32,485,906,774,888,179],(function(){return _=99813,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
